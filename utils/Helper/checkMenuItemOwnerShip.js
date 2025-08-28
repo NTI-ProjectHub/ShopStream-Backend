974d286a80de9d375a882a47087f8808b82b27bd
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-08-28 20:16:33 +0300

    fix check menu dep

--- utils/Helper/checkMenuItemOwnerShip.js
+++ utils/Helper/checkMenuItemOwnerShip.js
@@ -1,6 +1,5 @@
-const HTTP_STATUS = require('../constants/httpStatus');
-const MESSAGES = require('../constants/messages');
-const Restaurant = require('../models/restaurant.model');
+const MESSAGES = require('../../constants/messages');
+const Restaurant = require('../../models/restaurant.model');
 
 
 // Helper function for ownership checking
@@ -12,15 +11,15 @@ exports.checkMenuItemOwnership = async (user, restaurantId) => {
     if (user.role === "restaurant") {
         const userRestaurant = await Restaurant.findOne({ userId: user._id });
         if (!userRestaurant) {
-            return { authorized: false, status: HTTP_STATUS.FORBIDDEN, message: "User restaurant not found" };
+            return { authorized: false, message: "User restaurant not found" };
         }
 
         if (restaurantId.toString() !== userRestaurant._id.toString()) {
-            return { authorized: false, status: HTTP_STATUS.FORBIDDEN, message: MESSAGES.AUTHORIZATION_ERROR };
+            return { authorized: false, message: MESSAGES.AUTHORIZATION_ERROR };
         }
 
         return { authorized: true };
     }
 
-    return { authorized: false, status: HTTP_STATUS.FORBIDDEN, message: "Insufficient permissions" };
+    return { authorized: false, message: "Insufficient permissions" };
 }
