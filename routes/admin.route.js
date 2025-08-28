88f2d6d7d2ec8ae10edd68e854e81072e72e3a75
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-08-28 15:24:30 +0300

    Approve request added

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -9,7 +9,8 @@ router.get('/restaurants', authenticate, roleCheck(['admin']), adminController.g
 router.get('/restaurants/:id', authenticate, roleCheck(['admin']), adminController.getRestaurant);
 router.get('/restaurant-requests', authenticate, roleCheck(['admin']), adminController.getAllRestaurantRequests);
 router.get('/restaurant-requests/:id', authenticate, roleCheck(['admin']), adminController.getRestaurantRequest);
-router.post('/restaurant-requests/:id', authenticate, roleCheck(['admin']), adminController.rejectRestaurantRequest);
+router.post('/restaurant-requests/:id/approve', authenticate, roleCheck(['admin']), adminController.approveRestaurantRequest);
+router.post('/restaurant-requests/:id/reject', authenticate, roleCheck(['admin']), adminController.rejectRestaurantRequest);
 router.delete('/users/:id', authenticate, roleCheck(['admin']), adminController.deleteUser);
 router.delete('/restaurants/:id', authenticate, roleCheck(['admin']), adminController.deleteRestaurant);
 
