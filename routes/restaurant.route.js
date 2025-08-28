54ccd87427e42006434c46293ffc23cd3d8c2573
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-08-28 23:19:34 +0300

    restaurant fix

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -3,13 +3,16 @@ const restaurantController = require('../controllers/restaurant.controller');
 const router = express.Router();
 const { authenticate } = require('../middlewares/authentication.middleware');
 const { roleCheck } = require('../middlewares/authorization.middleware');
+const upload = require('../config/multer.Config');
+const cloud = require('../middlewares/cloud');
 
 // 🔹 Restaurants
 router.get('/', authenticate, restaurantController.getAllRestaurants);
 router.get('/:id', authenticate, restaurantController.getRestaurantById);
 
 // 🔹 Restaurant Requests
-router.post('/requests', authenticate, roleCheck(['restaurant']), restaurantController.createRestaurantRequest);
+router.post('/requests', authenticate, roleCheck(['restaurant']), upload.single('coverImage'), cloud.uploadCloud, restaurantController.createRestaurantRequest);
+
 router.delete('/requests/:id', authenticate, roleCheck(['restaurant']), restaurantController.removeRestaurantRequest);
 
 module.exports = router;
