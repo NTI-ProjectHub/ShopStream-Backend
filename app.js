6c631980cd2db0001f020a5019cb0ae1295a98fe
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-09-25 01:16:11 +0300

    fix cors

--- app.js
+++ app.js
@@ -27,7 +27,13 @@ const allowedOrigins = [
 
 
 app.use(cors({
-  origin: allowedOrigins, // Specify your Angular app’s origin
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
